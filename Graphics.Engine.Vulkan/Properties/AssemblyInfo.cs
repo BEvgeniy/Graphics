dbe3dd518d3ee78a2c9361416db6d08dff6ca322
Evgeniy <evgeniy.bytsenko@outlook.com>
2017-08-05 22:00:09 +0300

    Изменил свойства проектов

--- Graphics.Engine.Vulkan/Properties/AssemblyInfo.cs
+++ Graphics.Engine.Vulkan/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Graphic.Engine.Vulkan")]
+[assembly: AssemblyTitle("Graphics.Engine.Vulkan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Graphic.Engine.Vulkan")]
+[assembly: AssemblyProduct("Graphics.Engine.Vulkan")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
